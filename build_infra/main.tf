d3080d5c1ca9f78296302df7c453b77db4e82248
Prabin Gautam <prabin.gautam@rackspace.com>
2025-05-01 14:25:05 -0500

    more typos

--- build_infra/main.tf
+++ build_infra/main.tf
@@ -121,7 +121,7 @@ module "alb" {
   security_group_egress_rules = {
     all = {
       ip_protocol = "-1"
-      cidr_ipv4   = vars.cidr
+      cidr_ipv4   = vars.cidr_block
     }
   }
   security_group_description = "Allow http/https to ALB"
